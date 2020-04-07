dd4d34b08a2057f75a59c6d48f1f730c9846b4b1
srank123 <jaturong.duang@gmail.com>
2020-04-08 00:12:32 +0700

    first commit

---
+++ addons/openacademy/models/course.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
